ab588a1ad26cb491f166f9f69fc3e006e4c76215
scott2mu <89548124+scott2mu@users.noreply.github.com>
2021-12-08 21:34:38 -0500

    Update Project2.ps1

--- Project2/Project2.ps1
+++ Project2/Project2.ps1
@@ -1,29 +1,30 @@
+#Project 2 Murray Scott
 Add-Type -AssemblyName System.Speech
-$guesscount = 0
+$guesscount = 0 #initialize ints
 $attemptcount = 0
-$number = Get-Random -minimum 1 -maximum 50
-$voice = new-object System.Speech.Synthesis.SpeechSynthesizer
+$number = Get-Random -minimum 1 -maximum 50 #generate random number between 1 and 50
+$voice = new-object System.Speech.Synthesis.SpeechSynthesizer #voice that speaks given text,
 write-output "Guessing Game. Guess a number between 1 and 50. You have 5 tries."
 $voice.speak("Guessing Game. Guess a number between 1 and 50. You have 5 tries.")
-do {
+do { #do until loop
     $voice.speakasync("Please Guess") | Out-Null
 	$guesscount = read-host "What's your guess"
-    if ($guesscount -lt $number) {
+    if ($guesscount -lt $number) { #output if number is too low
 		write-output "Too Low"
         $voice.speak("Too low")
     }
-    elseif ($guesscount -gt $number) {
+    elseif ($guesscount -gt $number) { #output if number is too high
 		write-output "Too High"
         $voice.speak("Too high")
     }
     $attemptcount += 1
 }
-until ($guesscount -eq $number -or $attemptcount -eq 5)
-if ($guesscount -eq $number) {
+until ($guesscount -eq $number -or $attemptcount -eq 5) #output if number is correct
+if ($guesscount -eq $number) { #text/voice output if number is correct
 	write-output "You guessed $guesscount correctly. Good Game. You guessed in $attemptcount tries."
     $voice.speak("You guessed $guesscount correctly. Good Game. You guessed in $attemptcount tries.")
 }
-else {
+else { #text/voice output if number guesses reach the five limit with no correct guess
 	write-output "You lost. The number was $number. Hahahaha!"
     $voice.speak("You lost. The number was $number. Hahahaha")
 }
